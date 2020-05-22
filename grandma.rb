179838304dd24b3220087aaa12a882aa487b4f88
Sarah Peters <sarahpieters15@gmail.com>
2020-05-22 14:08:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
     return "NO, NOT SINCE 1938!"
-  else 
+  else
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
